1f39577c785f823e2c34900440a51a05ebe9c100
harshgehlot-code <er.harsh.gehlot1@gmail.com>
2026-03-12 21:19:18 +0530

    admin-login created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/normal/HomePage"
 import AboutPage from "./pages/normal/AboutPage"
 import CareerPage from "./pages/normal/CareerPage"
 import ServicePage from "./pages/normal/ServicePage"
+import AdminLogin from "./pages/admin/AdminLogin";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Route path="/about-page" element={<AboutPage />} />
       <Route path="/career-page" element={<CareerPage />} />
       <Route path="service-page" element={<ServicePage />} />
+      <Route path="/admin-login" element={<AdminLogin />} />
     </Routes>
   )
 }
